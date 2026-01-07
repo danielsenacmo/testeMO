247471a3d1f2fcf04bf981bb01a0c2570c67e994
danielsenacmo <danielsenacmo@gmail.com>
2026-01-07 16:36:53 -0300

    Primeiro commit

---
+++ ExemploMOUC11/src/main/java/com/mycompany/exemplomouc11/ExemploMOUC11.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplomouc11;
+
+/**
+ *
+ * @author Admin
+ */
+public class ExemploMOUC11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
